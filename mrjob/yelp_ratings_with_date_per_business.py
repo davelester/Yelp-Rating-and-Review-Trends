057dfce330f980dc6ab32a88374e1088f1719d76
Dave Lester <dave@ischool.berkeley.edu>
2012-04-17 21:28:16 -0700

    Add mapreduce job that outputs ratings with dates for each business.

---
+++ mrjob/yelp_ratings_with_date_per_business.py
@@ -0,0 +1,36 @@
+"""
+Output a list of star ratings and date for each business ID
+
+"""
+
+from mrjob.job import MRJob
+from mrjob.protocol import JSONValueProtocol
+from itertools import izip
+
+class MRCountYelpReviewsPerBusinesses(MRJob):
+	INPUT_PROTOCOL = JSONValueProtocol
+
+	def mapper(self, _, review):
+		if review['type'] == 'review':
+			biz_id = review['business_id']
+			rating = review['stars']
+			date = review['date']
+			yield biz_id, (rating, date)
+
+	def reducer(self, biz_id, value):
+		ratings, dates = izip(*value)
+
+		counter = 0
+		for rating in ratings:
+			yield biz_id, (dates[counter], rating)
+			counter += 1
+
+	def finale(self, key, value):
+		yield key, list(value)
+
+	def steps(self):
+		return [self.mr(self.mapper, self.reducer),
+				self.mr(reducer=self.finale)]
+
+if __name__ == '__main__':
+	MRCountYelpReviewsPerBusinesses.run()
